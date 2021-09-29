962d9a3937c36c0fe9eac91d7f3469aad508cb0c
JeanMg26 <jeanmg25@gmail.com>
2021-09-29 17:43:25 -0500

    web

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,11 +8,11 @@ import "bootstrap";
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+   window.Popper = require('popper.js').default;
+   window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
-} catch (e) {}
+   require('bootstrap');
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -35,8 +35,10 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+   broadcaster: 'pusher',
+   key: process.env.MIX_PUSHER_APP_KEY,
+   cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+   wsHost: 'websockets.la',
+   wsPort: 6001,
+   forceTLS: false,
 });
